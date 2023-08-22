d334142b5e4f4bb418e8e51b3a42ca018a2e17f4
satyam <satyambsingh93@gmail.com>
2023-08-22 19:27:31 +0530

    new

---
+++ pg3/app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('home')#value should a string
+
+def about(request):
+    return HttpResponse('about')
+    
+def contact(request):
+    return HttpResponse('contact')
+
